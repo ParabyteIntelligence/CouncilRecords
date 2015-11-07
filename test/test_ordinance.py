b241314362f8d55e9d70d953ed650a13b096b4d8
RakshakTalwar <rakshak@raptorbird.com>
2015-11-07 14:40:48 -0600

    writing test

---
+++ test/test_ordinance.py
@@ -0,0 +1,9 @@
+"""
+Copyright (c) Parabyte Intelligence LLC 2015
+Author: Rakshak Talwar
+
+Description: Tests the ordinance.py file
+"""
+
+import sys
+from source.ordinance import OrdinanceDocument
