ba0e4754c40ecc23ddb8be93a0e8540d77f103f0
Micah Thomas <micahthomas4@gmail.com>
2015-11-20 16:45:38 -0600

    use $location host for api

--- client/src/app/components/services/records.js
+++ client/src/app/components/services/records.js
@@ -6,7 +6,7 @@
     .service('Records', Records);
 
   /** @ngInject */
-  function Records($http) {
+  function Records($http, $location) {
     var service = {
       search: search
     };
@@ -15,7 +15,7 @@
 
     function search(query) {
 
-      return $http.get('http://localhost:9099/search', {
+      return $http.get('http://' + $location.host() + ':9099/search', {
         params: query
       }).then(function(res) {
         return res.data;
