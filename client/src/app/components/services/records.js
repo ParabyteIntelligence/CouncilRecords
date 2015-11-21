862692640c6eeb52ad866dd41d6b110eaf9c1865
Micah Thomas <micahthomas4@gmail.com>
2015-11-21 01:14:26 -0600

    added num_hits

--- client/src/app/components/services/records.js
+++ client/src/app/components/services/records.js
@@ -15,8 +15,24 @@
 
     function search(query) {
 
+      var params = {};
+      if (query.start_date && query.start_date !== 'Invalid date') {
+        params.start_date = query.start_date;
+      }
+      if (query.end_date && query.end_date !== 'Invalid date') {
+        params.end_date = query.end_date;
+      }
+      params.search_query = query.search_query;
+      if (params.min_amount) {
+        min_amount = query.min_amount;
+      }
+      if (params.max_amount) {
+        max_amount = query.max_amount;
+      }
+      params.num_hits = 50;
+
       return $http.get('http://' + $location.host() + ':8080/search', {
-        params: query
+        params: params
       }).then(function(res) {
         return res.data;
       });
